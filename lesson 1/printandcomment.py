8e546f09bdac7f1c025949436fb11f640282ac3b
nimraejaz12345 <nimraejaz893@gmail.com>
2026-04-23 15:54:35 +0500

    Add files via upload

---
+++ lesson 1/printandcomment.py
@@ -0,0 +1,13 @@
+# Printing a greeting message
+print ("Hello, world!")
+
+# Printing my nmae 
+print ("My name is Alex")
+
+# Printing a fun fact about myself
+# print("I love playing badminton.")
+print("I love playing badminton.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
