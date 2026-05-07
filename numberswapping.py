fa1ff0693e7193661f0d1b72d3f73f565fd37fca
nimraejaz12345 <nimraejaz893@gmail.com>
2026-05-07 13:24:09 +0500

    Add files via upload

---
+++ numberswapping.py
@@ -0,0 +1,13 @@
+# Input three numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# Swapping
+a, b, c = c, a, b
+
+# Output
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
